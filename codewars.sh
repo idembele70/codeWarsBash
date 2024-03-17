e712046dab5024c5911d8e028c66b1acca50cbf6
idembele70 <idembele70@gmail.com>
2024-03-17 22:04:06 +0100

    feat: 7kuy Complementary DNA in one line

--- codewars.sh
+++ codewars.sh
@@ -1,16 +1,3 @@
 DNA="ATTGC"
-Result=""
-for ((i = 0; i < ${#DNA}; i++)); do
-  if [ ${DNA:i:1} = "A" ]; then
-    result="${result}T"
-  elif [ ${DNA:i:1} = "T" ]; then
-    result="${result}A"
-    elif [ ${DNA:i:1} = "G" ]
-    then
-    result="${result}C"
-  else
-    result="${result}G"
-  fi
-done
 
-echo $result
+echo "$DNA" | tr "ATGC" "TACG"
