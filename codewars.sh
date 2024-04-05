7f6d8e9ff7d37fa43f9833da2a99064f917455c3
idembele70 <idembele70@gmail.com>
2024-04-05 13:09:47 +0200

    feat: #7kuy Mumbling

--- codewars.sh
+++ codewars.sh
@@ -1,4 +1,20 @@
-position=$1
-roll=$(($2 * 2))
+accum () {
+str=$1
+length=${#str}
+newStr=""
+for((i=0; i < length; i++)); do
+  currentChar="${str:i:1}"
+  charRepetition=""
+  for((j=0; j <= i; j++)); do
+    charRepetition="$charRepetition${currentChar,,}"
+  done
+  if [ -z "$newStr" ]; then
+    newStr="${charRepetition^}"
+  else
+    newStr="$newStr-${charRepetition^}"
+  fi
+done
+echo $newStr
+}
 
-echo $((position + roll))
+accum "$1"
